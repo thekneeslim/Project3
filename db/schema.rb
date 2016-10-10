42b3f4cc784659acebccae72fff04cffc2a391f4
Eugene Oei <eugeneoei89@gmail.com>
2016-10-10 18:14:54 +0800

    added profile page

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20161010093455) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
     t.string   "linkedin"
     t.string   "github"
     t.string   "website"
@@ -71,8 +73,6 @@ ActiveRecord::Schema.define(version: 20161010093455) do
     t.text     "description"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.string   "email"
     t.boolean  "admin"
     t.integer  "event_id"
     t.index ["event_id"], name: "index_students_on_event_id", using: :btree
