92dfbc16c8172f8094ba9f726b1318d173d6b358
Denise Lim <denise.lim.sh@gmail.com>
2016-10-10 14:20:17 +0800

    added in admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def is_admin
+    unless current_student.admin
+      flash[:danger] = "Credentials Invalid!! Admin access only!"
+      redirect_to login_path
+    end
+  end
+
   def current_student
     @current_student ||= Student.find_by_id(session[:student_id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010055123) do
+ActiveRecord::Schema.define(version: 20161010061318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20161010055123) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "email"
+    t.boolean  "admin"
   end
 
 end
