c255de37ab812a740dda316e47fd68e7477584aa
Eugene Oei <eugeneoei89@gmail.com>
2016-10-13 01:20:34 +0800

    added project gif column to project table

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -80,6 +80,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :github, :site, :description)
+      params.require(:project).permit(:name, :github, :site, :description, :project_gifs)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012025324) do
+ActiveRecord::Schema.define(version: 20161012131316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,9 @@ ActiveRecord::Schema.define(version: 20161012025324) do
     t.string   "github"
     t.string   "site"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "project_gifs"
   end
 
   create_table "projects_students", id: false, force: :cascade do |t|
