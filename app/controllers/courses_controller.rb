c4cc3ae3912a9a0923aa931b1a0807da2e08f64d
Denise Lim <denise.lim.sh@gmail.com>
2016-10-14 01:56:53 +0800

    adding in data

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -77,6 +77,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:name, :description, :picture_url)
+      params.require(:course).permit(:name, :description, :picture_url, :links)
     end
 end
