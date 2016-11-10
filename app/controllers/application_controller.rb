0f9e1603355c3e2ca933ebc5151ed9966bfd4902
Denise Lim <denise.lim.sh@gmail.com>
2016-11-11 02:08:58 +0800

    trying to solve deployment problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :current_student
-  puts "Current student: #{@current_student}"
+  # puts "Current student: #{@current_student}"
 
   def is_authenticated
     unless current_student
